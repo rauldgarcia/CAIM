8d1abfd27055083d21139115a35cd8e54a74504a
Raul Garcia <rauld.garcia95@gmail.com>
2023-04-12 17:45:06 -0600

    Se agrega calculo de b y d automatico y uso de pandas

--- caim.py
+++ caim.py
@@ -1,14 +1,50 @@
-dv=[0.2, 0.1, 1.4, 2.5, 1.8, 2.2, 1.3, 2.1, 1.9, 1.5]
+import numpy as np
+import pandas as pd
 
-dv.sort()
+data=pd.read_csv('prueba.csv')
+print(data)
 
+dataordenada=data.sort_values('valor')
+print(dataordenada)
+
+dv=list(dataordenada['valor'])
+print(dv)
+clases=list(dataordenada['clase'])
+clas=list(set(clases)) #crea lista de valores de clase unicos
+print(clas)
+
+#obtiene limites
 d0=dv[0]
 dn=dv[-1]
 
 b=[]
-print(b)
 for i in range(len(dv)-1):
     b.append(dv[i])
     b.append((dv[i]+dv[i+1])/2)
-b.append(dv[-1])
+#b.append(dv[-1])
+b.pop(0)
 print(b)
+
+daux=[d0,dn]
+d=[[d0,dn]]
+globalcaim=0
+k=1
+print()
+
+for elemento in b:
+    daux.pop()
+    daux.append(elemento)
+    daux.append(dn)
+    print("B:")
+    print(daux)
+    d=[]
+    for indice in range(len(daux)-1):
+        d.append([daux[indice],daux[indice+1]])
+    print("D:")
+    print(d)
+
+'''for i in dv:
+    quanta=np.zeros((len(clas),2+len(d)))
+    
+
+print(quanta)'''
