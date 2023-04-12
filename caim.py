d8818a281b5cd7e0dc51218c0fed890ded69d8c5
Raul Garcia <rauld.garcia95@gmail.com>
2023-04-11 21:07:09 -0600

    Se agrega obtencion de B

--- caim.py
+++ caim.py
@@ -1,11 +1,14 @@
 dv=[0.2, 0.1, 1.4, 2.5, 1.8, 2.2, 1.3, 2.1, 1.9, 1.5]
-print(dv)
 
 dv.sort()
-print(dv)
 
 d0=dv[0]
 dn=dv[-1]
 
-print(d0)
-print(dn)
+b=[]
+print(b)
+for i in range(len(dv)-1):
+    b.append(dv[i])
+    b.append((dv[i]+dv[i+1])/2)
+b.append(dv[-1])
+print(b)
